65a35986cf053ddec6321272b8e6587a446603ca
SamEBae <baesammie@gmail.com>
2017-02-14 22:31:51 -0800

    Problems-Recursion: Adds base10ToBinary

---
+++ Problems/Recursion/base10ToBinary.js
@@ -0,0 +1,18 @@
+function base10ToString(n) {
+    var binaryString = "";
+
+    function base10ToStringHelper(n) {
+        if (n < 2) {
+            binaryString += n;
+            return;
+        } else {
+            base10ToStringHelper(Math.floor(n / 2));
+            base10ToStringHelper(n % 2);
+        }
+    }
+    base10ToStringHelper(n);
+
+    return binaryString;
+}
+
+console.log(base10ToString(232)); // 11101000
