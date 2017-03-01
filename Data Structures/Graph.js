370e407fd610ab08c551b7689d24f03d30c423d8
Sammie Bae <sammie@goshippo.com>
2017-03-01 00:16:52 -0800

    Fixes dijkstra algorithms

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -1,7 +1,18 @@
-/**
- * Basic Graph data structure implementation
- * @constructor
- */
+// Helper
+function _extractMin(Q, dist) {
+    var minimumDistance = Infinity;
+    var nodeWithMinimumDistance;
+
+    for (var node in Q) {
+        if (dist[node] <= minimumDistance) {
+            minimumDistance = dist[node];
+            nodeWithMinimumDistance = node;
+        }
+    }
+    return nodeWithMinimumDistance;
+}
+
+// Main
 var Graphs = function() {
     this.vertices = {};
 };
@@ -15,51 +26,39 @@ Graphs.prototype.length = function(u, v) {
     return (this.vertices[u][v]);
 };
 
-/**
- * Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph
- * @param source
- * @returns {{}}
- */
+// Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph
+
 Graphs.prototype.Dijkstra = function(source) {
     // create vertex set Q
-    var Q    = {},
+    var Q = {},
         dist = {},
         prev = {};
 
-    /**
-     * for each vertex v in Graph:             // Initialization
-     *     dist[v] ← INFINITY                  // Unknown distance from source to v
-     *     prev[v] ← UNDEFINED                 // Previous node in optimal path from source
-     *     add v to Q                          // All nodes initially in Q (unvisited nodes)
-     */
     for (var vertex in this.vertices) {
+        // unknown distance set to Infinity
         dist[vertex] = Infinity;
         prev[vertex] = undefined;
+        // add v to Q
         Q[vertex] = this.vertices[vertex];
     }
-
-    // dist[source] ← 0  // Distance from source to source
-
+    // Distance from source to source init to 0
     dist[source] = 0;
 
     // while Q is not empty:
     while (!_isEmpty(Q)) {
-        // u ← vertex in Q with min dist[u]    // Source node will be selected first
+        // u ← vertex in Q with min dist[u]    
         var u = _extractMin(Q, dist);
 
         // remove u from Q
         delete Q[u];
 
-        // for each neighbor v of u:           // where v is still in Q.
+        // for each neighbor v of u:
+        // where v is still in Q.
         for (var neighbor in this.vertices[u]) {
             // alt ← dist[u] + length(u, v)
             var alt = dist[u] + G.length(u, neighbor);
 
-            /**
-             * if alt < dist[v]:               // A shorter path to v has been found
-             *     dist[v] ← alt
-             *     prev[v] ← u
-             */
+            // A shorter path to neighbor has been found
             if (alt < dist[neighbor]) {
                 dist[neighbor] = alt;
                 prev[neighbor] = u;
@@ -71,7 +70,16 @@ Graphs.prototype.Dijkstra = function(source) {
 
 // Usage
 var G = new Graphs();
-G.add('S', { V: 1, W: 4 });
-G.add('W', { T: 3 });
-G.add('V', { W: 2, T: 6 });
+G.add('S', {
+    V: 1,
+    W: 4
+});
+G.add('W', {
+    T: 3
+});
+G.add('V', {
+    W: 2,
+    T: 6
+});
 G.add('T');
+console.log(G.Dijkstra('S')); // {"S": 0, "T":6, "V":1, "W": 3}
